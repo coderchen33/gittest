b98a83a4568bf4d7316652bb3a38a7eddfcaf12c
coderchen33 <lchenxjtu@163.com>
2021-07-18 10:45:52 +0800

    第二次修改

--- Hello.java
+++ Hello.java
@@ -2,5 +2,6 @@ public class Hello{
     public static void main(String[] args){
         System.out.println("Hello git");
         System.out.println("第一次修改");
+        System.out.println("第二次修改");
     }
 }
